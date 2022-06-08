17fd4ebf07fbe5e907740d0e52302b43481e52d3
mauriprado <104120014+mauriprado@users.noreply.github.com>
2022-06-08 12:56:51 -0500

    feat(notifications): Added Notification Service.

---
+++ SocialMed.API/Notifications/Domain/Services/Communication/NotificationResponse.cs
@@ -0,0 +1,16 @@
+﻿using SocialMed.API.Notifications.Domain.Models;
+using SocialMed.API.Shared.Domain.Services.Comunication;
+
+namespace SocialMed.API.Notifications.Domain.Services.Communication;
+
+public class NotificationResponse: BaseResponse<Notification>
+{
+    public NotificationResponse(string message) : base(message)
+    {
+    }
+
+    public NotificationResponse(Notification resource) : base(resource)
+    {
+        
+    }
+}
