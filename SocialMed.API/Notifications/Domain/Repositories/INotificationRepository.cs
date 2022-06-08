5e478d86a7e5c7ea1a1fe4413ed3fb4ce92cb5ea
mauriprado <104120014+mauriprado@users.noreply.github.com>
2022-06-08 12:48:56 -0500

    feat(notifications): Added and implemented Notification Repository.

---
+++ SocialMed.API/Notifications/Domain/Repositories/INotificationRepository.cs
@@ -0,0 +1,12 @@
+﻿using SocialMed.API.Notifications.Domain.Models;
+
+namespace SocialMed.API.Notifications.Domain.Repositories;
+
+public interface INotificationRepository
+{
+    Task<IEnumerable<Notification>> ListAsync();
+    Task AddAsync(Notification notification);
+    Task<Notification> FindByIdAsync(int id);
+    void Update(Notification notification);
+    void Remove(Notification notification);
+}
--- SocialMed.API/SocialMedCenter/Persistence/Context/AppDbContext.cs
+++ SocialMed.API/SocialMedCenter/Persistence/Context/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SocialMed.API.Forums.Domain.Models;
+using SocialMed.API.Notifications.Domain.Models;
 using SocialMed.API.Shared.Extensions;
 using SocialMed.API.SocialMedCenter.Domain.Models;
 
@@ -7,13 +8,14 @@ namespace SocialMed.API.SocialMedCenter.Persistence.Context;
 
 public class AppDbContext : DbContext
 {
-    public DbSet<Chat> Chats { get; set; }  // review logical
+    /*public DbSet<Chat> Chats { get; set; }  // review logical
     public DbSet<Message> Messages { get; set; }
     
     public DbSet<Comment> Comments { get; set; }
     public DbSet<Forum> Forums { get; set; }
     public DbSet<Rating> Ratings { get; set; }
-    public DbSet<User> Users { get; set; }
+    public DbSet<User> Users { get; set; }*/
+    public DbSet<Notification> Notifications;
 
     public AppDbContext(DbContextOptions options) : base(options)
     {
@@ -23,7 +25,7 @@ public class AppDbContext : DbContext
     {
         base.OnModelCreating(builder);
 
-        builder.Entity<Rating>().ToTable("Ratings");
+        /*builder.Entity<Rating>().ToTable("Ratings");
         builder.Entity<Rating>().HasKey(p => p.Id);
         builder.Entity<Rating>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
         
@@ -51,7 +53,7 @@ public class AppDbContext : DbContext
 
 
         /////////////////////////////////////////////////////
-        builder.Entity<User>().ToTable("Users");
+        /*builder.Entity<User>().ToTable("Users");
         builder.Entity<User>().HasKey(p => p.Id);
         builder.Entity<User>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
         builder.Entity<User>().Property(p => p.Name).IsRequired().HasMaxLength(50);
@@ -109,9 +111,14 @@ public class AppDbContext : DbContext
             .HasForeignKey(p => p.ForumId);
         
 
-        ////////////////////////////////////////////////////
-        
-        
+        ////////////////////////////////////////////////////*/
+
+        builder.Entity<Notification>().ToTable("Notifications");
+        builder.Entity<Notification>().HasKey(p => p.Id);
+        builder.Entity<Notification>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Notification>().Property(p => p.Title).IsRequired().HasMaxLength(30);
+        builder.Entity<Notification>().Property(p => p.UserId).IsRequired();
+
         // Apply Snake Case Naming Convention
         
         builder.UseSnakeCaseNamingConvention();
